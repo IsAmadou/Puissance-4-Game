337609802fcd60969bdbebec1834a0754840924c
IsAmadou <Amadou99isssaka@outlook.fr>
2021-01-06 04:29:37 +0100

    Add non-OK IA level 5

--- src/helper/IAAmadou.java
+++ src/helper/IAAmadou.java
@@ -21,9 +21,10 @@ public class IAAmadou extends Joueur {
 	//	int num_colonne = niveau_un(pl);
 	//	int num_colonne = niveau_deux(pl, joueur);
 	//	int num_colonne = niveau_trois(pl, joueur);
-		int num_colonne = niveau_quatre(pl, joueur);
-		
-		
+	//	int num_colonne = niveau_quatre(pl, joueur);
+
+		int num_colonne = niveau_cinq(pl, joueur);
+	
 		return num_colonne;	
 	}
 	
@@ -179,7 +180,124 @@ public class IAAmadou extends Joueur {
 	}
 
 	
-	
+	public static  int niveau_cinq(Plateau pl, int joueur) {
+		List<String> list_colonne= new ArrayList<String>();
+		List<Plateau> neou = new ArrayList<Plateau>();
+		List<Case> neouAjout = new ArrayList<Case>();
+		List<Plateau> feuil = new ArrayList<Plateau>();
+		
+		int num_adversaire = 0;
+		if(joueur==1) {
+			num_adversaire=2;
+		}else if(joueur==2) {
+			num_adversaire=1;
+		}
+		//Lister les colonnes ou on peut jouer
+		for(int i=0;i<pl.getTab_col();i++) {
+			if(pl.colonnePleine(i).equals("vide")) {
+				list_colonne.add(String.valueOf(i));
+			}
+		}
+		
+		//Simuler un ajout sur chacune des colonnes
+		for (String colonne_libre : list_colonne) {
+			Plateau p1 = new Plateau(pl.toStringPourIA());	
+			p1.changeValueInPlateau();
+			Case c = p1.ajoutPion(Integer.parseInt(colonne_libre), joueur);
+			 neou.add(p1);
+			 neouAjout.add(c);
+		}	
+		
+		//Lister les colonnes ou l adversaire peut jouer apres le 1er ajouts
+		 for (Plateau p : neou) {
+			List<String> list_sous_colonne= new ArrayList<String>();
+			for(int i=0;i<p.getTab_col();i++) {
+				if(p.colonnePleine(i).equals("vide")) {
+					list_sous_colonne.add(String.valueOf(i));
+				}
+			}
+			
+			//Simuler un ajout adverse sur chacune des colonnes 
+			for (String sous_colonne_libre : list_sous_colonne) {
+				Plateau p2 = new Plateau(p.toStringPourIA());		
+				p2.changeValueInPlateau();
+				Case sous_c = p2.ajoutPion(Integer.parseInt(sous_colonne_libre), num_adversaire);
+				feuil.add(p2);
+				if(p2.ValeurDuJeu(IA, sous_c.getCoordx(), sous_c.getCoordy())==278) {
+					return sous_c.getCoordy();
+				}
+				p2.getXY(sous_c.getCoordy(),sous_c.getCoordx()).setContenu(num_adversaire);
+			}
+		}
+		 //Si l adversaire ne peut pas joueur on retourne la colonne avec la plus grande valeur
+		 if(feuil.isEmpty()) {
+			 int i=0, first_val =279,col = pl.getTab_col();
+			 for (Plateau p : neou) {
+				int val = p.ValeurDuJeu(IA, neouAjout.get(i).getCoordx(), neouAjout.get(i).getCoordy());
+				if(i==0) {
+					first_val=val;
+					col=neouAjout.get(i).getCoordy();
+				}
+				if(val>first_val) {
+					first_val=val;
+					col=neouAjout.get(i).getCoordy();
+				}
+				i++;
+			}
+			 return col;
+		 }
+		 
+		 List<String> cols = new ArrayList<String>();
+		 List<String> vals = new ArrayList<String>();
+		 int first_val = 0;
+		for (Plateau p : feuil) {
+			//Lister les colonnes ou on peut jouer apres le 1er ajouts adverse
+			List<String> list_sous_colonne= new ArrayList<String>();
+			for(int i=0;i<p.getTab_col();i++) {
+				if(p.colonnePleine(i).equals("vide")) {
+					list_sous_colonne.add(String.valueOf(i));
+				}
+			}
+				int j =0;
+			//Simuler un ajout sur chacune des colonnes et calculer valeur de jeu
+			for (String sous_colonne_libre : list_sous_colonne) {
+				Plateau p3 = new Plateau(p.toStringPourIA());	
+				p3.changeValueInPlateau();
+				Case sous_c = p3.ajoutPion(Integer.parseInt(sous_colonne_libre), joueur);
+				int val=-(p3.ValeurDuJeu(IA,sous_c.getCoordx(), sous_c.getCoordy()));
+				if(j==0) {
+					first_val=val;
+					vals.add(String.valueOf(val));
+					cols.add(sous_colonne_libre);
+					j=1;
+				}
+				if(val<first_val) {
+					first_val=val;
+					vals.set((vals.size()-1), String.valueOf(val));
+					cols.set((cols.size()-1),sous_colonne_libre);
+				}
+			}
+		}
+		int colonne = pl.getTab_col();
+		int first=Integer.parseInt(vals.get(0));
+		for(int i=0;i<vals.size();i++) {
+			if(Integer.parseInt(vals.get(i))<0 && first<0) {
+				if(Integer.parseInt(vals.get(i))<=first) {
+					first=Integer.parseInt(vals.get(i));
+					colonne=Integer.parseInt(cols.get(i));
+				}
+			}else {
+				if(Integer.parseInt(vals.get(i))>=first && Integer.parseInt(vals.get(i))!=-277 && first>=0) {
+					first=Integer.parseInt(vals.get(i));
+					colonne=Integer.parseInt(cols.get(i));
+				}
+			}
+		}
+		
+		return colonne ; 
+		
+		
+	}
 	
 }
 
